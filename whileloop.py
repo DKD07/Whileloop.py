b46324521beac10bf28bf8f3385d971ca285bd79
DKD07 <161266358+DKD07@users.noreply.github.com>
2024-03-07 23:50:21 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,22 @@
+i=0
+while(i<3):
+    print(i)
+    i=i+1
+print("#######")
+while(i<=3):
+    print(i)
+    i=i+1
+print("#######")
+i=int(input("Enter the number:"))
+while(i<=38):
+    i=int(input("Enter the number: "))
+    print(i)
+print("Done with the loop")
+print("########")
+#Decrementing while loop
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("I am in insdie")
